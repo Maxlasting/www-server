e013bf348c2c24825c1d0893709f6323b5cbbdba
FuQiang <best.fq.master@gmail.com>
2020-03-13 22:39:45 +0800

    -

--- routes/default.route.js
+++ routes/default.route.js
@@ -1,9 +1,27 @@
 const { prefix, get } = require(__base + 'decorators')
 
+const fs = require('fs')
+const util = require('util')
+const readFile = util.promisify(fs.readFile)
+const { join } = require('path')
+const send = require('koa-send')
+
 @prefix('/')
 class __defaultRouter__ {
-  @get('*')
+  @get('/www/*')
   async _route_01_ (ctx) {
-    ctx.body = 'ok'
+    console.log(ctx.path)
+    await send(ctx, ctx.path)
+  }
+
+  @get('*')
+  async _route_00_ (ctx) {
+    ctx.type = 'text/html'
+
+    const html = await readFile(
+      join(__dirname, '..', 'www', 'index.html')
+    )
+
+    ctx.body = html
   }
 }
